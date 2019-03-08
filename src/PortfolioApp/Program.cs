231600198c4b72ffaf0bc5300873aad56c18473d
Gavin Wilson <gavinwilson@gmail.com>
2019-03-07 22:23:35 -0800

    Questrade client can get a response from a hardcoded URL.

--- src/PortfolioApp/Program.cs
+++ src/PortfolioApp/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
-using QuestradeClient;
+using System.Threading.Tasks;
+using PortfolioSmarts.Questrade;
 
-namespace PortfolioApp
+namespace PortfolioSmarts.PortfolioApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var client = new QuestradeClientBloop();
-            client.GetStuff().Wait();
+            var program = new Program();
+            program.ExecuteClient().Wait();
+        }
+
+        private readonly QuestradeClient _client;
+        private Program()
+        {
+            _client = new QuestradeClient();
+        }
+
+        private async Task ExecuteClient()
+        {
+            var stuff = await _client.GetStuff();
+            Console.WriteLine(stuff);
             Console.WriteLine("Done");
+            return;
         }
     }
 }
---
+++ src/Questrade/QuestradeClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace PortfolioSmarts.Questrade
+{
+    public class QuestradeClient : HttpClient
+    {
+        public QuestradeClient()
+            : base()
+        {
+
+        }
+
+        public async Task<string> GetStuff()
+        {
+            this.DefaultRequestHeaders.Add("User-Agent", "PortfolioSmarts-v0");
+
+            var stringTask = this.GetStringAsync("https://www.google.com");
+            var msg = await stringTask;
+            return msg;
+        }
+    }
+}
