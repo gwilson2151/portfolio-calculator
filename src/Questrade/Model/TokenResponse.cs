e771efe010263449f9b0aa20a81eec97d2ab549e
Gavin Wilson <gavinwilson@gmail.com>
2019-03-08 21:32:23 -0800

    [WIP] Deserialise access token response. API not working...

---
+++ src/Questrade/Model/TokenResponse.cs
@@ -0,0 +1,19 @@
+using System.Runtime.Serialization;
+
+namespace PortfolioSmarts.Questrade.Model
+{
+    [DataContract]
+    public class TokenResponse
+    {
+        [DataMember(Name="refresh_token")]
+        public string RefreshToken { get; set; }
+        [DataMember(Name="access_token")]
+        public string AccessToken { get; set; }
+        [DataMember(Name="token_type")]
+        public string TokenType { get; set; }
+        [DataMember(Name="api_server")]
+        public string ApiServer { get; set; }
+        [DataMember(Name="expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
--- src/Questrade/QuestradeClient.cs
+++ src/Questrade/QuestradeClient.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
+using PortfolioSmarts.Questrade.Model;
+
 namespace PortfolioSmarts.Questrade
 {
     public class QuestradeClient : HttpClient
     {
+        private string _refreshToken;
+        private string _accessToken;
+        private string _apiUrl;
+        private string _tokenType;
+        private DateTime _tokenRequestTime;
+        private TimeSpan _expiry;
+
         public QuestradeClient()
             : base()
         {
@@ -20,10 +30,22 @@ namespace PortfolioSmarts.Questrade
             var contentBody = $"grant_type=refresh_token&refresh_token={refreshToken}";
             var message = new StreamContent(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(contentBody)));
             message.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            _tokenRequestTime = DateTime.UtcNow;
+            Console.WriteLine("Requesting");
             var response = await this.PostAsync("https://login.questrade.com/oauth2/token", message);
-
+            Console.WriteLine("Responded");
             Console.WriteLine(await response.Content.ReadAsStringAsync());
-            Console.WriteLine($"{response.StatusCode} {response.ReasonPhrase}");
+
+            var serializer = new DataContractJsonSerializer(typeof(TokenResponse));
+            var result = serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as TokenResponse;
+            Console.WriteLine("Deserialised");
+
+            Console.WriteLine(result.ToString());
+            _refreshToken = result.RefreshToken;
+            _accessToken = result.AccessToken;
+            _tokenType = result.TokenType;
+            _expiry = new TimeSpan(0, 0, result.ExpiresIn);
+            _apiUrl = result.ApiServer;
 
             return;
         }
